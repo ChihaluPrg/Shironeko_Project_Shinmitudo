d31a98b765d1b523dc926f6bb569ade518380262
ChihaluPrg <saki.prg.20@gmail.com>
2024-11-22 18:54:55 +0900

    全ての処理の実装

---
+++ Main.py
@@ -0,0 +1,8 @@
+from OpenCV2 import *
+from Quest import *
+
+if __name__ == "__main__":
+    time.sleep(1)
+    print("親密度上げスクリプトを起動しました\n")
+
+    process_quest()
---
+++ Quest.py
@@ -0,0 +1,166 @@
+from OpenCV2 import *
+
+quest_13_3_path = "img/quest_13_3.jpg"
+go_path = "img/go.jpg"
+item_kouka_path = "img/item_kouka.jpg"
+quest_rule_path = "img/ok_kakunin.jpg"
+item_selection_path = "img/item_selection.jpg"
+boost_item_path = "img/boost_2bai.jpg"
+max_path = "img/max.jpg"
+item_use_path = "img/item_use.jpg"
+result_ok_path = "img/result_ok.jpg"
+retry_path = "img/retry.jpg"
+retry_yes_path = "img/retry_yes.jpg"
+go_room_ok_path = "img/go_room_ok.jpg"
+go_room_path = "img/go_room.jpg"
+
+
+
+def process_quest():
+    while True :
+        position = find_template_position(quest_13_3_path)
+        if position:
+            tap_on_device(position[0], position[1])
+            print("クエスト：飛来せし岩を破壊せよ！を選択しました")
+            time.sleep(1)
+            break
+        else:
+            print("クエスト：飛来せし岩を破壊せよ！を選択できませんでした...再試行します...")
+
+    while True :
+        position = find_template_position(go_path)
+        if position:
+            tap_on_device(position[0], position[1])
+            print("挑戦ボタンをタップしました")
+            time.sleep(13)
+            break
+        else:
+            print("挑戦ボタンをタップできませんでした...再試行します...")
+
+    try:
+        while True :
+            while True:
+                position = find_template_position(quest_rule_path)
+                if position:
+                    tap_on_device(position[0], position[1])
+                    print("クリア条件をタップしました")
+                    time.sleep(17)
+                    break
+                else:
+                    print("クリア条件をタップできませんでした")
+
+            position = find_template_position(item_kouka_path)
+            if position:
+                print("アイテム効果は切れていません")
+                time.sleep(2)
+            else:
+                print("アイテム効果が切れている為、アイテムを使用する処理を行います")
+                time.sleep(3)
+                while True:
+                    position = find_template_position(result_ok_path)
+                    if position:
+                        tap_on_device(position[0], position[1])
+                        print("OKボタンをタップしました")
+                        time.sleep(7)
+                        item_use()
+                        break
+                    else:
+                        print("OKボタンをタップできませんでした")
+
+            while True:
+                position = find_template_position(go_room_path)
+                if position:
+                    print("キャラの親密度が上がりました")
+                    while True:
+                        position = find_template_position(go_room_ok_path)
+                        if position:
+                            tap_on_device(position[0], position[1])
+                            print("OKボタンをタップしました")
+                            time.sleep(2)
+                            break
+                        else:
+                            print("OKボタンをタップできませんでした")
+                    break
+                else:
+                    break
+
+            while True:
+                position = find_template_position(retry_path)
+                if position:
+                    tap_on_device(position[0], position[1])
+                    time.sleep(1.5)
+                    print("再挑戦ボタンをタップしました")
+                    break
+                else:
+                    print("再挑戦ボタンをタップできませんでした...再試行します...")
+
+            while True:
+                position = find_template_position(retry_yes_path)
+                if position:
+                    tap_on_device(position[0], position[1])
+                    print("はいボタンをタップしました")
+                    time.sleep(15)
+                    break
+                else:
+                    print("はいボタンをタップできませんでした")
+    except KeyboardInterrupt:
+        print("ループを終了しました")
+
+
+def item_use():
+    while True :
+        position = find_template_position(quest_13_3_path)
+        if position:
+            tap_on_device(position[0], position[1])
+            print("クエスト：飛来せし岩を破壊せよ！を選択しました")
+            time.sleep(2)
+            break
+        else:
+            print("クエスト：飛来せし岩を破壊せよ！を選択できませんでした...再試行します...")
+
+    while True :
+        position = find_template_position(item_selection_path)
+        if position:
+            tap_on_device(position[0], position[1])
+            print("アイテム選択をタップしました")
+            break
+        else:
+            print("アイテム選択をタップできませんでした")
+
+    while True :
+        position = find_template_position(boost_item_path)
+        if position:
+            tap_on_device(position[0], position[1])
+            print("冒険入門書～親密度～を選択しました")
+            break
+        else:
+            print("アイテムを選択できませんでした...再試行します...")
+
+    while True :
+        position = find_template_position(go_path)
+        if position:
+            tap_on_device(position[0], position[1])
+            print("挑戦ボタンをタップしました")
+            while True:
+                position = find_template_position(item_use_path)
+                if position:
+                    tap_on_device(position[0], position[1])
+                    print("冒険入門書～親密度～を使用しました")
+                    break
+                else:
+                    print("アイテムを使用できませんでした...再試行します...")
+            time.sleep(13)
+            break
+        else:
+            print("挑戦ボタンをタップできませんでした...再試行します...")
+
+    while True:
+        while True:
+            position = find_template_position(quest_rule_path)
+            if position:
+                tap_on_device(position[0], position[1])
+                print("クリア条件をタップしました")
+                time.sleep(17)
+                break
+            else:
+                print("クリア条件をタップできませんでした")
